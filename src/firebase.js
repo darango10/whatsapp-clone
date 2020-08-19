dd50d8f66fd3360441363e10e3fa9d3746a49682
Daniel <arangomonsalve.daniel@gmail.com>
2020-08-19 15:07:41 -0500

    App init

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAczEY9Bxc2zei4keOobOVPK_-FpHhztpA",
+    authDomain: "clone-whatsapp123.firebaseapp.com",
+    databaseURL: "https://clone-whatsapp123.firebaseio.com",
+    projectId: "clone-whatsapp123",
+    storageBucket: "clone-whatsapp123.appspot.com",
+    messagingSenderId: "1047865062778",
+    appId: "1:1047865062778:web:c992a19c07da178dfcd2c3",
+    measurementId: "G-PV8XGGBV45"
+};
